61197ebc361533d96faddb7910bc27faee4ba8e0
anju-jojer <59391581+anju-jojer@users.noreply.github.com>
2021-07-10 22:33:16 +0530

    Update index

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
 
   <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.3/themes/hot-sneaks/jquery-ui.css" />
-	<script src="http://code.jquery.com/jquery-2.1.3.js"></script>
-	<script src="http://code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.3/themes/hot-sneaks/jquery-ui.css" />
+	<script src="https://code.jquery.com/jquery-2.1.3.js"></script>
+	<script src="https://code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
